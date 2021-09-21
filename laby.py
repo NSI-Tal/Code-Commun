0c45a56516c654532390f0d3d6d6b7b1e28c7c98
GregoireALP <gregoire.alperovitch@gmail.com>
2021-09-21 12:16:30 +0200

    Update Repo

--- laby.py
+++ laby.py
@@ -1,35 +1,4 @@
-from copy import deepcopy
-import matplotlib as plt
-
-laby = [[0,1,0,0,0,0],
-        [0,1,1,1,1,0],
-        [0,1,0,1,0,0],
-        [0,1,0,1,1,0],
-        [0,1,1,0,1,0],
-        [0,0,0,0,1,0]]
-
-T = deepcopy(laby)
-T[3][2] = 'hello'
-
-for ligne in laby:
-    print(ligne)
-print("----------------")
-for ligne in T:
-    print(ligne)
-lignes= len(laby)
-colonnes = len(laby[0])
-
-def voisins(T,v):  # sourcery skip: merge-nested-ifs
-    V = []
-    i,j = v[0],v[1]
-    for a in (-1,1):
-        if 0<= i + a < lignes:
-            if T[i+a][j] == 1:
-                V.append((i+a,j))
-        if 0<= j + a <colonnes:
-            if T[i][j+a] == 1:
-                V.append((i,j+a))
-    return V
-
-print(voisins(laby,(0,0)))
-
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
