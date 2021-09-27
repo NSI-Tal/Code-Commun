2a5b9716c74f7dc562e30d446cead018a72863d1
GregoireALP <gregoire.alperovitch@gmail.com>
2021-09-27 19:27:39 +0200

    27092021

--- dst1/exercice4.py
+++ dst1/exercice4.py
@@ -1,4 +1,4 @@
-def apartient(val,liste):
+def apartient(val, liste):
     if val==liste[0]:
         return True
     if len(liste)==0:
@@ -8,6 +8,7 @@ def apartient(val,liste):
 
 def nbr_de_chiffres(n):
     mot = str(n)
+
     if mot == "":
         return 0
         
@@ -16,13 +17,17 @@ def nbr_de_chiffres(n):
 nb1 = 0
 def nbr_de_bit(n):
     global nb1
+
     def binaire(nb):
         global nb1
-        if nb==0:
-            return""
-        nb1=nb1+1
-        return binaire(nb//2)+str(nb%2)
-    binaire(n)
-    return nb1
 
-print(nbr__de_bit(42))#niquez vos chiennes
+        if nb == 0:
+            return""
+
+        nb1 = nb1+1
+        
+        return binaire(nb//2)+str(nb%2)
+
+    binaire(n)
+
+    return nb1
